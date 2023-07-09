934860e7ac92712606498d41d24b7459737c258e
aziz-codes <azizisoffline@gmail.com>
2023-07-09 07:30:16 +0500

    changed flex row to flex-col

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Feed from "./components/Feed";
 const App = () => {
   return (
     <section className="w-screen flex justify-center">
-      <div className="w-full max-w-6xl flex justify-center mt-12">
+      <div className="w-full max-w-6xl flex flex-col items-center space-y-6 justify-center mt-12">
         <Feed />
       </div>
     </section>
