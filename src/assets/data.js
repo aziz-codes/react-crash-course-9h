a8d777c31c15764d0a865e2701e8768c010bcd65
aziz-codes <azizisoffline@gmail.com>
2023-07-09 07:31:15 +0500

    added some images, and are added to gitignore

---
+++ src/assets/data.js
@@ -0,0 +1,20 @@
+import boy1 from "./images/boy1.jpg";
+import girl1 from "./images/girl1.jpg";
+import girl2 from "./images/girl2.jpg";
+import headphone from "./images/headphone.jpg";
+import john from "./images/john.jpg";
+import kid1 from "./images/kid1.jpg";
+export const dummy = [
+  {
+    username: "azizcodes",
+    images: [boy1],
+  },
+  {
+    username: "anas",
+    images: [girl1, girl2, headphone],
+  },
+  {
+    username: "asad",
+    images: [john, kid1],
+  },
+];
