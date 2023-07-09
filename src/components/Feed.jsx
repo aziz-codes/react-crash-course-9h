3ef8736ba8c956a9c4195d05d7f42a809869c053
aziz-codes <azizisoffline@gmail.com>
2023-07-09 07:30:49 +0500

    rendering data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { dummy } from "../assets/data";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <>
+      {dummy.map((item, index) => (
+        <section
+          className="w-96 max-w-sm border flex flex-col p-1 rounded-sm"
+          key={index}
+        >
+          <h4 className="text-xs font-semibold">@{item.username}</h4>
+
+          {item.images.map(
+            (image, i) =>
+              item.images.length === 2 && (
+                <div className="flex flex-row w-full" key={i}>
+                  <img
+                    src={image}
+                    alt={item.username}
+                    className="w-1/2 h-full"
+                  />
+                </div>
+              )
+          )}
+        </section>
+      ))}
+    </>
+  );
 };
 
 export default Feed;
