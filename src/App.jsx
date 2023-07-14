98f01321ad4f2f56c2630280c3622e9d71af26f4
aziz-codes <azizisoffline@gmail.com>
2023-07-14 20:27:09 +0500

    changed w-full of main wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Feed from "./components/Feed";
 
 const App = () => {
   return (
-    <section className="w-screen flex justify-center">
-      <div className="w-full max-w-6xl flex flex-col items-center space-y-6 justify-center mt-12">
+    <section className="w-full flex justify-center overflow-hidden">
+      <div className="w-full max-w-6xl flex flex-col items-center space-y-6 justify-center mt-12 my-4">
         <Feed />
       </div>
     </section>
