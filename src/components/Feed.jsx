c5b0171da4cdffd2cb222ce1b8303c801bc351f3
aziz-codes <azizisoffline@gmail.com>
2023-07-14 20:24:25 +0500

    more than 3 images layout completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -24,7 +24,7 @@ const Feed = () => {
               ))}
             </div>
           )}
-          {item.images.length > 2 && (
+          {item.images.length === 3 && (
             <div className="flex flex-row gap-0.5">
               <img
                 src={item.images[0]}
@@ -40,6 +40,37 @@ const Feed = () => {
                   src={item.images[2]}
                   className="h-1/2 w-full object-center"
                 />
+              </div>
+            </div>
+          )}
+
+          {item.images.length > 3 && (
+            <div className="flex flex-row gap-0.5">
+              <img
+                src={item.images[0]}
+                className="h-full w-1/2 rounded-sm object-center object-cover"
+                alt="girl"
+              />
+              <div className="flex flex-col h-full gap-0.5">
+                <img
+                  src={item.images[1]}
+                  className="h-1/2 w-full object-center"
+                />
+
+                <div
+                  className="relative h-1/2 w-full object-center flex justify-center 
+                items-center "
+                >
+                  <img
+                    src={item.images[item.images.length - 1]}
+                    className="h-full w-full -z-1"
+                  />
+                  <div className="absolute h-full w-full bg-opacity-90 bg-gray-400 flex justify-center items-center">
+                    <h4 className="text-4xl text-gray-800">
+                      +{item.images.length}
+                    </h4>
+                  </div>
+                </div>
               </div>
             </div>
           )}
