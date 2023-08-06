5df76a00a1a0fe7adf222c8697e25add160e84ac
aziz-codes <azizisoffline@gmail.com>
2023-08-06 21:18:18 +0500

    importing components, removed previous feed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Feed from "./components/Feed";
+import Test1 from "./components/Test1";
+import Test2 from "./components/Test2";
+import UpdateStoreCount from "./components/UpdateStoreCount";
 
 const App = () => {
   return (
-    <section className="w-full flex justify-center overflow-hidden">
-      <div className="w-full max-w-6xl flex flex-col items-center space-y-6 justify-center mt-12 my-4">
-        <Feed />
+    <section className="w-full flex flex-col items-center justify-center overflow-hidden gap-2">
+      <UpdateStoreCount />
+      <div className="flex gap-6">
+        <Test1 />
+        <Test2 />
       </div>
     </section>
   );
