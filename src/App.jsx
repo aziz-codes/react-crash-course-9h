9acfbaa3c2d9edc62f92eb5ee1f02d5ae7b80cf1
aziz-codes <azizisoffline@gmail.com>
2023-09-16 20:55:06 +0500

    working with form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import Toast from "./layouts/Toast";
-import { useEffect, useState } from "react";
+import Forms from "./Methods/Forms";
+// import Toast from "./layouts/Toast";
+// import { useEffect, useState } from "react";
 const App = () => {
-  const [showToast, setShowToast] = useState(false);
-  const handleClick = () => setShowToast((prev) => !prev);
+  // const [showToast, setShowToast] = useState(false);
+  // const handleClick = () => setShowToast((prev) => !prev);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowToast(false);
-    }, 5000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowToast(false);
+  //   }, 5000);
 
-    return () => clearTimeout(timer);
-  }, [showToast]);
+  //   return () => clearTimeout(timer);
+  // }, [showToast]);
 
   return (
-    <div>
-      <button
-        className="m-12 border bg-sky-500 px-2 py-2 rounded-md hover:ring-1"
-        onClick={handleClick}
-      >
-        Show Toast
-      </button>
+    // <div>
+    //   <button
+    //     className="m-12 border bg-sky-500 px-2 py-2 rounded-md hover:ring-1"
+    //     onClick={handleClick}
+    //   >
+    //     Show Toast
+    //   </button>
 
-      <Toast display={showToast} content="Some text goes here" />
-    </div>
+    //   <Toast display={showToast} content="Some text goes here" />
+    // </div>
+    <Forms />
   );
 };
 
---
+++ src/Methods/Forms.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useRef } from "react";
+
+const Forms = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [data, setData] = useState([]);
+  const handleClick = () => {
+    let newData = {
+      [emailRef.current.name]: emailRef.current.value,
+      [passwordRef.current.name]: passwordRef.current.value,
+    };
+    setData(newData);
+    console.log(data);
+  };
+  console.log(data);
+  return (
+    <div className="w-full h-screen relative flex justify-center items-center">
+      <div className="h-auto py-4 max-w-sm w-96 rounded-md shadow-lg bg-white flex flex-col px-2 gap-4">
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-gray-400">
+            email
+          </label>
+          <input
+            ref={emailRef}
+            type="text"
+            placeholder="Email"
+            className="rounded-md outline-none border px-1 py-1"
+            id="email"
+            autoComplete="false"
+            name="email"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-400" htmlFor="password">
+            name
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="rounded-md outline-none border px-1 py-1"
+            id="password"
+            name="password"
+            ref={passwordRef}
+          />
+        </div>
+        <button
+          className="outline-none px-4 py-1.5 rounded-md text-black border  mx-auto focus:shadow-inner"
+          onClick={handleClick}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Forms;
